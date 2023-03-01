5b0e8ac04dcd2709055f05ab8478b07592ea8b4f
ldeleon1306 <45015209+ldeleon1306@users.noreply.github.com>
2023-03-01 14:22:59 -0300

    Update Worker.cs

--- Worker.cs
+++ Worker.cs
@@ -23,7 +23,7 @@ namespace WorkerConecMongo
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                var client = new MongoClient("mongodb://10.20.2.46:27017");
+                var client = new MongoClient("mongodb://10.20.2.46:27017/authdb?serverSelectionTimeoutMS=3000&connectTimeoutMS=3000&socketTimeoutMS=3000");
                 Console.WriteLine("conecto a mongo 21");
                 Console.WriteLine("entra a mongo 22");
                 List<string> NombrebaseDatos = client.ListDatabaseNames().ToList();
