db3e89311d8b68574a782efd3084304a3cd7dbd4
ldeleon1306 <45015209+ldeleon1306@users.noreply.github.com>
2023-03-01 15:54:42 -0300

    Add files via upload

--- Worker.cs
+++ Worker.cs
@@ -32,36 +32,45 @@ namespace WorkerConecMongo
                 var database = client.GetDatabase("APIAlmacenes");
                 Console.WriteLine(database);
                 Console.WriteLine("entrando al mail");
-                MailMessage mail = new MailMessage();
-                //Console.WriteLine(_mailFrom.ToString());
-                //Console.WriteLine(_mailTo);
-                mail.From = new MailAddress("appdesabrdcsrv@andreani.com");
-                mail.To.Add("ldeleon@andreani.com");
-                //var multiple = _mailTo.Split(';');
-                //foreach (var to in multiple)
-                //{
-                //    if (to != string.Empty)
-                //        mail.To.Add(to);
-                //}              
-                System.Net.Mail.Attachment attachment;
-                //attachment = new System.Net.Mail.Attachment("output.txt");
-                //mail.Attachments.Add(attachment);
-                //if (!String.IsNullOrEmpty(txterror))
-                //{
-                //  attachment = new System.Net.Mail.Attachment(txterror);
-                //  mail.Attachments.Add(attachment);
-                //}
-                string cliente = "prueba";
-                string subject = string.Format($"Prueba {cliente} Mail");
-                string bodyMsg = string.Format($"Se procesó un mongo {cliente}");
-                mail.Subject = subject;
-                mail.Body = bodyMsg;
-                mail.IsBodyHtml = true;
-                Console.WriteLine(bodyMsg);
-                SmtpClient smtp = new SmtpClient("10.20.2.41");
-                smtp.EnableSsl = false;
-                smtp.Port = 25;
-                smtp.UseDefaultCredentials = true;
+                try
+                {
+                    MailMessage mail = new MailMessage();
+                    //Console.WriteLine(_mailFrom.ToString());
+                    //Console.WriteLine(_mailTo);
+                    mail.From = new MailAddress("appdesabrdcsrv@andreani.com");
+                    mail.To.Add("ldeleon@andreani.com");
+                    //var multiple = _mailTo.Split(';');
+                    //foreach (var to in multiple)
+                    //{
+                    //    if (to != string.Empty)
+                    //        mail.To.Add(to);
+                    //}              
+                    System.Net.Mail.Attachment attachment;
+                    //attachment = new System.Net.Mail.Attachment("output.txt");
+                    //mail.Attachments.Add(attachment);
+                    //if (!String.IsNullOrEmpty(txterror))
+                    //{
+                    //  attachment = new System.Net.Mail.Attachment(txterror);
+                    //  mail.Attachments.Add(attachment);
+                    //}
+                    string cliente = "prueba";
+                    string subject = string.Format($"Prueba {cliente} Mail");
+                    string bodyMsg = string.Format($"Se procesó un mongo {cliente}");
+                    mail.Subject = subject;
+                    mail.Body = bodyMsg;
+                    mail.IsBodyHtml = true;
+                    Console.WriteLine(bodyMsg);
+                    SmtpClient smtp = new SmtpClient("10.20.2.41");
+                    smtp.EnableSsl = false;
+                    smtp.Port = 25;
+                    smtp.UseDefaultCredentials = true;
+                }
+                catch (Exception ex)
+                {
+
+                    throw ex;
+                }
+               
 
                 await Task.Delay(1000, stoppingToken);
             }
